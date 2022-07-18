e7359c0d51047fa6fe80e00761843b587f476858
Jennifer Trevillian <95890754+JCPTrevillian@users.noreply.github.com>
2022-07-18 14:22:46 -0700

    misc corrections

--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,21 @@ const express = require('express');
 const router = express.Router();
 const { projects } = require('../data.json');
 
-//root route 
+ 
 router.get('/', (req, res) => {
 	res.render('index', { projects });
 });
 
-// about route 
+ 
 router.get('/about', (req, res) => {
 	res.render('about');
 });
 
-// project route based on id 
+
 router.get('/project/:id', (req, res, next) => {
 	const projectId = req.params.id;
 	const project = projects.find(({ id }) => id === +projectId);
-	/** If the project can be found - render the project template and pass it the project object 
-        Else return next which will trigger the 404 error handler
-    */
+	
 	if (project) {
 		res.render('project', { project });
 	} else {
@@ -26,7 +24,7 @@ router.get('/project/:id', (req, res, next) => {
 	}
 });
 
-// prevent 404
+
 router.get('/favicon.ico', (req, res) => {
 	res.sendStatus(204);
 });
